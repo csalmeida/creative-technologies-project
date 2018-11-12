69dcc2bfaaab60ba13c7bf03b155a4792a8d516b
cristiano <cs.almeida@protonmail.ch>
2018-11-12 13:04:15 +0000

    Draws keypoints from pose data.

--- src/Components/PoseNet/index.js
+++ src/Components/PoseNet/index.js
@@ -13,26 +13,41 @@ async function estimatePoseOnImage(imageElement) {
 }
 
 class PoseNet extends Component {
+  drawKeypoints(keypoints) {
+    const canvas = this.canvas
+    const context = canvas.getContext("2d")
+    for (let i = 0; i < keypoints.length; i++) {
+      context.beginPath()
+      context.arc(
+        keypoints[i].position.x,
+        keypoints[i].position.y,
+        5,0,2*Math.PI)
+      context.fill()
+    }
+  }
+
   componentDidMount() {
     // running the code here is not working, try something else
     let pose = estimatePoseOnImage(this.image)
+    .then((data) => {
+      // Drawing has to occur when promise is fulfilled.
+      this.drawKeypoints(data.keypoints)
+    })
     console.log("Pose Estimation...")
     console.log(this.image)
     console.log("Pose data:", pose)
 
-    This code runs a video stream.
-    const video = this.video
-    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    .then(function(stream) {
-        video.srcObject = stream;
-        video.play();
-        console.log("Video is playing from component");
-    })
-    .catch(function(err) {
-        console.log("An error occurred! " + err);
-    });
-
-
+    // This code runs a video stream.
+    // const video = this.video
+    // navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    // .then(function(stream) {
+    //     video.srcObject = stream
+    //     video.play()
+    //     console.log("Video is playing from component")
+    // })
+    // .catch(function(err) {
+    //     console.log("An error occurred! " + err)
+    // })
   }
 
   render() {
@@ -56,9 +71,12 @@ class PoseNet extends Component {
             height="500"
           >Video stream is not available.</video>
           <canvas
+            ref={ canvas => {
+                this.canvas = canvas
+            }}
             width="500"
             height="500"
-          ></canvas>
+          >Canvas is not available in this browser.</canvas>
       </Fragment>
     )
   }
