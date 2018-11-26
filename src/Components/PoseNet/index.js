374fee1f287b17d562cc238d775fbdd88b54483c
cristiano <cs.almeida@protonmail.ch>
2018-11-26 14:15:05 +0000

    Wrote implementation using ml5 and p5. Canvas size needs fixing.

--- src/Components/PoseNet/index.js
+++ src/Components/PoseNet/index.js
@@ -1,117 +1,79 @@
 import React, { Component, Fragment } from 'react'
-import testFrame1 from '../../Assets/Images/test_frame_1_square.png'
-import * as posenet from '@tensorflow-models/posenet'
-const imageScaleFactor = 0.5;
-const outputStride = 16;
-const flipHorizontal = false;
+import * as p5 from 'p5'
+import "p5/lib/addons/p5.dom";
+//import * as p5DOM from '../../Libraries/p5.dom'
+import * as ml5 from 'ml5'
 
-async function estimatePoseOnImage(imageElement) {
-  // load the posenet model from a checkpoint
-  const net = await posenet.load(0.75)
-  const pose = await net.estimateSinglePose(imageElement, imageScaleFactor, flipHorizontal, outputStride)
-  //const pose = await net.estimateMultiplePoses(imageElement, imageScaleFactor, flipHorizontal, outputStride)
-  return pose
+console.info("ML5", ml5)
+console.info("P5", p5)
+
+const draw = (sketch, video, poses) => {
+  sketch.image(video, 0, 0, video.width, video.height)
+  // We can call both functions to draw all keypoints and the skeletons
+  drawKeypoints(sketch, poses)
+  //this.drawSkeleton();
 }
+
+// A function to draw ellipses over the detected keypoints
+const drawKeypoints = (sketch, poses) => {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
+    // For each pose detected, loop through all the keypoints
+    let pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      if (keypoint.score > 0.2) {
+        sketch.fill(255,255,255);
+        sketch.noStroke();
+        sketch.ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      }
+    }
+  }
+}
+
+// A function to draw the skeletons
+// drawSkeleton = () => {
+//   // Loop through all the skeletons detected
+//   for (let i = 0; i < poses.length; i++) {
+//     let skeleton = poses[i].skeleton;
+//     // For every skeleton, loop through all body connections
+//     for (let j = 0; j < skeleton.length; j++) {
+//       let partA = skeleton[j][0];
+//       let partB = skeleton[j][1];
+//       stroke(c.r,c.g,c.b);
+//       line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+//     }
+//   }
+// }
 
 class PoseNet extends Component {
   constructor(props) {
     super(props);
-    this.image = React.createRef();
     this.video = React.createRef();
-    this.canvas = React.createRef();
-    this.screen = React.createRef();
-  }
-
-  drawKeypoints(keypoints) {
-    const canvas = this.canvas.current,
-          context = canvas.getContext("2d"),
-          radius = 2,
-          startAngle = 0,
-          endAngle = 2*Math.PI
-
-
-    for (let i = 0; i < keypoints.length; i++) {
-      context.beginPath()
-      context.arc(
-        keypoints[i].position.x,
-        keypoints[i].position.y,
-        radius,
-        startAngle,
-        endAngle)
-      context.fill()
-    }
-  }
-
-  drawFrame = (video) => {
-    const screen = this.screen.current
-    screen.height = 500
-    screen.width = 500
-    let context = screen.getContext('2d')
-    context.drawImage(
-      this.video.current,
-      0, 0,
-      screen.width, screen.height,
-      0, 0,
-      screen.width, screen.height)
-    requestAnimationFrame(this.drawFrame)
   }
 
   componentDidMount() {
-    //console.log("Screen data: ", this.screen.current.toDataURL);
+    const sketch = new p5()
+    sketch.createCanvas(640, 480)
+    const video = sketch.createCapture(p5.VIDEO)
+    video.size(sketch.width, sketch.height)
 
-    const image = this.image.current
-    const pose = estimatePoseOnImage(this.screen.current)
-    .then((data) => {
-      // Drawing has to occur when promise is fulfilled.
-      this.drawKeypoints(data.keypoints)
-      console.info("Points: ", data)
+
+    const poseNet = ml5.poseNet(video);
+    poseNet.on('pose', function(poses) {
+      console.log("Poses", poses)
+      draw(sketch, video, poses)
+      // save this to state poses = results;
     })
-    console.log("Pose Estimation...")
-    console.log(image)
-    console.log("Pose data:", pose)
-
-    // This code runs a video stream.
-    const video = this.video.current
-    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    .then((stream) => {
-        video.srcObject = stream
-        video.play()
-        video.addEventListener("play", this.drawFrame(video), false);
-        console.log("Video is playing from component")
-    })
-    .catch(function(err) {
-        console.log("An error occurred! " + err)
-    })
-  }
-
-  componentDidUpdate() {
-
+    video.hide()
   }
 
   render() {
     return(
       <Fragment>
-        <p>Hi from Posenet.</p>
-        <img
-          ref={this.image}
-          src={testFrame1}
-          alt="Test frame."
-          width="500"
-          height="500"
-           />
-          <video
-            ref={this.video}
-            width="500"
-            height="500"
-          >Video stream is not available.</video>
-          <canvas
-            ref={this.screen}
-          >Canvas is not available in this browser.</canvas>
-          <canvas
-            ref={this.canvas}
-            width="500"
-            height="500"
-          >Canvas is not available in this browser.</canvas>
+        <p>Posenet</p>
       </Fragment>
     )
   }
