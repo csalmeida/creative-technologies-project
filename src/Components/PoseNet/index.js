ba54ae20234bf767dc649fe36a9f41cf4c6e8e66
cristiano <cs.almeida@protonmail.ch>
2018-11-29 03:08:58 +0000

    Sets correct of canvas. Draws skeleton.

--- src/Components/PoseNet/index.js
+++ src/Components/PoseNet/index.js
@@ -8,10 +8,11 @@ console.info("ML5", ml5)
 console.info("P5", p5)
 
 const draw = (sketch, video, poses) => {
-  sketch.image(video, 0, 0, video.width, video.height)
+  sketch.resizeCanvas(sketch.windowWidth, sketch.windowHeight)
+  sketch.image(video, 0, 0)
   // We can call both functions to draw all keypoints and the skeletons
   drawKeypoints(sketch, poses)
-  //this.drawSkeleton();
+  drawSkeleton(sketch, poses)
 }
 
 // A function to draw ellipses over the detected keypoints
@@ -22,31 +23,31 @@ const drawKeypoints = (sketch, poses) => {
     let pose = poses[i].pose;
     for (let j = 0; j < pose.keypoints.length; j++) {
       // A keypoint is an object describing a body part (like rightArm or leftShoulder)
-      let keypoint = pose.keypoints[j];
+      let keypoint = pose.keypoints[j]
       // Only draw an ellipse is the pose probability is bigger than 0.2
       if (keypoint.score > 0.2) {
-        sketch.fill(255,255,255);
-        sketch.noStroke();
-        sketch.ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+        sketch.fill(255,255,255)
+        sketch.noStroke()
+        sketch.ellipse(keypoint.position.x, keypoint.position.y, 10, 10)
       }
     }
   }
 }
 
 // A function to draw the skeletons
-// drawSkeleton = () => {
-//   // Loop through all the skeletons detected
-//   for (let i = 0; i < poses.length; i++) {
-//     let skeleton = poses[i].skeleton;
-//     // For every skeleton, loop through all body connections
-//     for (let j = 0; j < skeleton.length; j++) {
-//       let partA = skeleton[j][0];
-//       let partB = skeleton[j][1];
-//       stroke(c.r,c.g,c.b);
-//       line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
-//     }
-//   }
-// }
+const drawSkeleton = (sketch, poses) => {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i++) {
+    let skeleton = poses[i].skeleton;
+    // For every skeleton, loop through all body connections
+    for (let j = 0; j < skeleton.length; j++) {
+      let partA = skeleton[j][0];
+      let partB = skeleton[j][1];
+      sketch.stroke(255,255,255);
+      sketch.line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+    }
+  }
+}
 
 class PoseNet extends Component {
   constructor(props) {
@@ -58,7 +59,10 @@ class PoseNet extends Component {
     const sketch = new p5()
     sketch.createCanvas(640, 480)
     const video = sketch.createCapture(p5.VIDEO)
+    console.log("width", sketch.width, "height", sketch.height);
+    console.log("P5 Video", sketch.VIDEO);
     video.size(sketch.width, sketch.height)
+
 
 
     const poseNet = ml5.poseNet(video);
