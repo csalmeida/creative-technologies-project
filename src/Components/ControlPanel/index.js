9deb62a59a983ca3e3fc511de1df2f0904b21b13
cristiano <cs.almeida@protonmail.ch>
2018-12-30 18:09:18 +0000

    Various fixes to Select.

--- src/Components/ControlPanel/index.js
+++ src/Components/ControlPanel/index.js
@@ -11,6 +11,7 @@ class ControlPanel extends Component {
         <Input label="Color" placeholder="#1b4c2a" />
         <Input label="Camera" type="checkbox" checked={true} />
         <Select label={"Output Stride"} options={[8, 16, 32]} />
+        <Select label={"Pose Detection"} options={['Single', 'Multiple']} />
       </Container>
     )
   }
--- src/Components/Select/styles.js
+++ src/Components/Select/styles.js
@@ -8,10 +8,11 @@ display: flex;
 flex-direction: row;
 justify-content: space-between;
 align-items: center;
+margin-bottom: 1rem;
 max-height: ${2.2 - 0.5 + "rem"};
 
 background: ${palette.midtone};
-padding: 0.5rem 0.5rem;
+padding: 0.6rem 0.5rem;
 border-radius: 0.4rem;
 
 label {
@@ -25,7 +26,7 @@ select {
 
 export const Options = styled.div`
 background: ${palette.darktone};
-padding: 0.4rem 0.5rem;
+padding: 0.5rem 0.5rem;
 border-radius: 0.4rem;
 cursor: pointer;
 position: relative;
@@ -37,10 +38,52 @@ select {
 ${props => props.open && 
     css`
     align-self: flex-start;
+    z-index: 10;
     `
 }
 `
 
+export const Option = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-content: center;
+  position: relative;
+
+  font-size: 1.2rem;
+  min-width: 5rem;
+  text-align: center;
+  padding-bottom: 0;
+  margin-bottom: 0;
+  border-bottom: none;
+
+  :not(:last-of-type) {
+    padding-bottom: 0.5rem;
+    margin-bottom: 0.5rem;
+    border-bottom: 2px solid ${palette.lighttone};
+  }
+
+${props => props.selected && 
+    css`
+    color: ${palette.highlight};
+    transition: color .2s;
+    `
+  }
+
+  svg {
+    fill: ${palette.highlight};
+    width:  ${iconSize * 1.8 + "rem"};
+    height: ${iconSize * 1.8 + "rem"};
+    cursor: pointer;
+    margin-left: 0.5rem;
+
+    ${ props => props.open ? (
+        css`transform: rotate(0deg);`
+      ) : (
+        css`transform: rotate(180deg);`
+      )}
+  }
+`
 
 export const Triangle = styled.div`
   position: relative;
@@ -79,48 +122,6 @@ export const Triangle = styled.div`
 
   :after {
     transform: rotate(135deg) skewY(-45deg) scale(.707,1.414) translate(${props => props.open ? 50 + "%" : -50 + "%"});
-  }
-`
-export const Option = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-content: center;
-  position: relative;
-
-  font-size: 1.2rem;
-  min-width: 5rem;
-  text-align: center;
-  padding-bottom: 0;
-  margin-bottom: 0;
-  border-bottom: none;
-
-  :not(:last-of-type) {
-    padding-bottom: 0.5rem;
-    margin-bottom: 0.5rem;
-    border-bottom: 2px solid ${palette.lighttone};
-  }
-
-${props => props.selected && 
-    css`
-    color: ${palette.highlight};
-    transition: color .2s;
-    `
-  }
-
-  svg {
-    position: absolute;
-    right: 0px;
-    fill: ${palette.highlight};
-    width:  ${iconSize * 1.8 + "rem"};
-    height: ${iconSize * 1.8 + "rem"};
-    cursor: pointer;
-
-    ${ props => props.open ? (
-        css`transform: rotate(0deg);`
-      ) : (
-        css`transform: rotate(180deg);`
-      )}
   }
 `
 
