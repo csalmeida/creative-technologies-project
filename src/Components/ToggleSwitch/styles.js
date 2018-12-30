b1183037678755fd3c96f7793b778332214c4d4d
cristiano <cs.almeida@protonmail.ch>
2018-12-30 14:43:52 +0000

    Created Toggle switch component.

---
+++ src/Components/ToggleSwitch/styles.js
@@ -0,0 +1,50 @@
+import styled, { css, keyframes } from 'styled-components'
+import { palette } from '../../Styles/colors'
+
+// Keeps Switch width propotionate to Circle.
+const circleWidth = 1;
+
+export const Switch = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: ${props => props.toggled ? "flex-end" : "flex-start"};
+  position: relative;
+  background: ${palette.darktone};
+  width: ${(circleWidth * 2.5) + "rem"};
+  padding: 0.2rem;
+  border-radius: 1rem;
+  cursor: pointer;
+`
+
+export const Circle = styled.div`
+  position: relative;
+  background: ${palette.highlight};
+  width: ${circleWidth + "rem"};
+  height: ${circleWidth + "rem"};
+  border-radius:50%;
+  animation: ${moveHorizontally};
+`
+
+const forwards = keyframes`
+  from {
+    left: 0;
+    
+  }
+  to {
+    left: ${(circleWidth * 2.5) - circleWidth + "rem"};
+  }
+`
+
+const reverse = keyframes`
+  from {
+    left: ${(circleWidth * 2.5) - circleWidth + "rem"};
+    
+  }
+  to {
+    left: 0;
+  }
+`
+
+const moveHorizontally = css`
+  animation: ${props => props.toggled ? forwards : reverse } .3s forwards 1;
+`
