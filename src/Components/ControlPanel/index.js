c7e853dbe17ad2d590cf58f235b4e6084de4a11b
cristiano <cs.almeida@protonmail.ch>
2018-12-30 19:36:14 +0000

    Created tabs, temporary control  styles.

--- src/Components/ControlPanel/index.js
+++ src/Components/ControlPanel/index.js
@@ -1,19 +1,95 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import Input from '../Input'
 import Select from '../Select'
-import { Container } from './styles'
+import { Container, Tabs, Tab } from './styles'
+
+const tabs = [
+  'Video Stream',
+  'Pose Estimation',
+  'Sound Mapping'
+]
 
 class ControlPanel extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      active: 0
+    }
+  }
+
+  setActive = (key) => {
+    this.setState({
+      active: key
+    })
+  }
+
+  renderVideoStreamFields = () => {
+    return(<Container>
+      <Input label="Camera" type="checkbox" checked={true} />
+      <Input label="Video" type="checkbox" checked={true} />
+      <Input label="Mirror" type="checkbox" checked={false} />
+      <Input label="Fullscreen" type="checkbox" checked={false} />
+      <Select label={"Facing Mode"} options={['User', 'Environment', 'Left', 'Right']} />
+      <Input label="Framerate" placeholder="fps" />
+    </Container>)
+  }
+
+  renderPoseEstimationFields = () => {
+    return(<Container>
+      <h1>Pose Estimation</h1>
+      <Input label="Skeleton" type="checkbox" checked={true} />
+      <Input label="Color" placeholder="#1b4c2a" />
+      <Input label="Flip Horizontal" type="checkbox" checked={false} />
+      <Select label={"Pose Detection"} options={['Single', 'Multiple']} />
+      <Input label="Image Scale Factor" placeholder="number" />
+      <Select label={"Output Stride"} options={[8, 16, 32]} />
+    </Container>)
+  }
+
+  renderSoundMappingFields = () => {
+    return(<Container>
+      <h1>Sound Mapping</h1>
+    </Container>)
+  }
+
+  renderFields = () => {
+    switch (this.state.active) {
+      case 0:
+        return this.renderVideoStreamFields()
+      
+      case 1:
+        return this.renderPoseEstimationFields()
+
+      case 2:
+        return this.renderSoundMappingFields()
+    
+      default:
+        return (<Container>
+          <h1>Control Panel</h1>
+          <Input label="Color" placeholder="#1b4c2a" />
+          <Input label="Camera" type="checkbox" checked={true} />
+          <Input label="Multiplier" type="number" checked={true} />
+          <Select label={"Output Stride"} options={[8, 16, 32]} />
+          <Select label={"Pose Detection"} options={['Single', 'Multiple']} />
+        </Container>)
+    }
+  }
+
   render = () => {
     return(
-      <Container>
-        <h1>Control Panel</h1>
-        <Input label="Color" placeholder="#1b4c2a" />
-        <Input label="Camera" type="checkbox" checked={true} />
-        <Input label="Multiplier" type="number" checked={true} />
-        <Select label={"Output Stride"} options={[8, 16, 32]} />
-        <Select label={"Pose Detection"} options={['Single', 'Multiple']} />
-      </Container>
+      <Fragment>
+      <Tabs>
+        {tabs.map((tab, index) => {
+          return <Tab
+          key={index} 
+          active={index === this.state.active}
+          onClick={() => this.setActive(index)}><h2>{tab}</h2></Tab>
+        })}
+      </Tabs>
+      {this.renderFields()}
+      </Fragment>
+      
     )
   }
 }
--- src/Components/ControlPanel/styles.js
+++ src/Components/ControlPanel/styles.js
@@ -1,8 +1,43 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { palette } from '../../Styles/colors'
+
+export const Tabs = styled.nav`
+display: flex;
+flex-direction: row;
+justify-content: flex-start;
+align-items: center;
+padding-top: 1rem;
+background: ${palette.darktone};
+`
+
+export const Tab = styled.div`
+  background: ${palette.midtone};
+  padding: 0.5rem 0.8rem;
+  cursor: pointer;
+  border-radius: 0.5rem 0.5rem 0 0;
+  
+  /* :not(:last-of-type) {
+    margin-right: 0.5rem;
+  } */
+  
+  h2 {
+    color: ${palette.text};
+    font-size: 1.2rem;
+  }
+
+  ${props => props.active &&
+    css`
+    background: ${palette.base};
+    `
+  }
+`
 
 export const Container = styled.div`
 display: flex;
 flex-direction: column;
 justify-content: center;
-align-items: center;
+align-items: space-between;
+flex-wrap: wrap;
+padding: 2rem 1rem;
 `
+
