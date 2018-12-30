6a6ba386e97446bdbd66b022ef618587f19145cc
cristiano <cs.almeida@protonmail.ch>
2018-12-30 14:45:23 +0000

    Created ControlPanel component.

---
+++ src/Components/ControlPanel/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Input from '../Input'
+import Select from '../Select'
+import { Container } from './styles'
+
+class ControlPanel extends Component {
+  render = () => {
+    return(
+      <Container>
+        <h1>Control Panel</h1>
+        <Input label="Color" placeholder="#1b4c2a" />
+        <Input label="Camera" type="checkbox" checked={true} />
+        <Select />
+      </Container>
+    )
+  }
+}
+
+export default ControlPanel
