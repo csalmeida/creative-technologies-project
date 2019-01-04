bd2737537b1aeabc958e50ee90797a492ca6523a
cristiano <cs.almeida@protonmail.ch>
2019-01-04 23:30:33 +0000

    Tweaked existing actions.

--- src/Store/Actions/index.js
+++ src/Store/Actions/index.js
@@ -1,10 +1,16 @@
-import { VIDEOSTREAM } from './types'
-
 // Action creator.
-export function cameraToggle(toggle) {
-  console.log("Video stream")
+export function alternate(toggle,type) {
+  console.log(`Video stream: ${type}`)
   return {
-    type: VIDEOSTREAM.CAMERA_TOGGLE,
+    type: type,
     payload: toggle
+  }
+}
+
+export function updateValue(value,type) {
+  console.log(`Video stream: ${type}`)
+  return {
+    type: type,
+    payload: value
   }
 }
