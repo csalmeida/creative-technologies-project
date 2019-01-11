15c928d683f8902139796b840a3efc6ab595428c
cristiano <cs.almeida@protonmail.ch>
2019-01-11 19:33:18 +0000

    Imported reducers and combined them.

--- src/Store/Reducers/index.js
+++ src/Store/Reducers/index.js
@@ -1,58 +1,8 @@
-import { VIDEOSTREAM } from '../Actions/types'
+import { combineReducers } from "redux"
+import videoStream from './videoStream'
+import poseEstimation from './poseEstimation'
 
-const rootReducer = (state = {}, action) => {
-  switch (action.type) {
-    case VIDEOSTREAM.CAMERA_TOGGLE:
-      return {
-        ...state,
-        videoStream: {
-          ...state.videoStream,
-          camera: !state.videoStream.camera
-        }
-      }
-      case VIDEOSTREAM.VIDEO_TOGGLE:
-      return {
-        ...state,
-        videoStream: {
-          ...state.videoStream,
-          video: !state.videoStream.video
-        }
-      }
-      case VIDEOSTREAM.MIRROR_TOGGLE:
-      return {
-        ...state,
-        videoStream: {
-          ...state.videoStream,
-          mirror: !state.videoStream.mirror
-        }
-      }
-      case VIDEOSTREAM.FULLSCREEN_TOGGLE:
-      return {
-        ...state,
-        videoStream: {
-          ...state.videoStream,
-          fullscreen: !state.videoStream.fullscreen
-        }
-      }
-      case VIDEOSTREAM.FACINGMODE_UPDATE:
-      return {
-        ...state,
-        videoStream: {
-          ...state.videoStream,
-          facingMode: action.payload
-        }
-      }
-      case VIDEOSTREAM.FRAMERATE_UPDATE:
-      return {
-        ...state,
-        videoStream: {
-          ...state.videoStream,
-          framerate: action.payload
-        }
-      }
-    default:
-      return state
-  }
-}
-
-export default rootReducer
+export default combineReducers({
+  videoStream,
+  poseEstimation,
+})
