ff7e81da1cb1e9af8305760437743c0a653aefef
cristiano <cs.almeida@protonmail.ch>
2019-01-13 15:22:31 +0000

    Image can now be mirrored.

--- src/Components/PoseNet/styles.js
+++ src/Components/PoseNet/styles.js
@@ -1,17 +1,19 @@
-import styled from 'styled-components'
-// import { palette } from '../../Styles/colors'
+import styled, { css } from 'styled-components'
+import { palette } from '../../Styles/colors'
 
 const Container = styled.div`
   width: 100%;
   border: red 2px solid;
   
   canvas {
-    
+    background: ${palette.darktone};
     border: blueviolet 2px solid;
+    ${props => props.mirror && css`
+      transform: scale(-1, 1) !important;
+    `}
   }
 
-    video {
-    
+  video {
     border: purple 2px solid;
   }
 `
