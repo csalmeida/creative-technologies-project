8dbf7a552be60b38f214853e8c666e98457236a5
cristiano <cs.almeida@protonmail.ch>
2019-02-09 00:52:32 +0000

    Note and envelope now receive parameters.

--- src/Functions/sound.js
+++ src/Functions/sound.js
@@ -2,9 +2,35 @@ import * as p5 from "p5"
 import "p5/lib/addons/p5.sound"
 
 // Creating a sound wave.
-const wave = new p5.Oscillator()
-wave.setType("sine")
-wave.start()
-wave.amp(0.1, 1)
-wave.freq(200)
-console.log("Wave", wave)
+export const singleNote = (options = { amplitude: false, frequency: 200 }) => {
+  const wave = new p5.Oscillator()
+  wave.setType("sine")
+  if (options.amplitude) {
+    wave.amp(0.1, 1)
+  } 
+  wave.freq(options.frequency)
+  wave.start()
+  return wave
+}
+
+export const createEnvelope = (options = {
+  attackLevel: 1.0,
+  releaseLevel: 0,
+  attackTime: 0.001,
+  decayTime: 0.3,
+  susPercent: 0.2,
+  releaseTime: 0.5
+}) => {
+  const envelope = new p5.Envelope()
+  envelope.setADSR(options.attackTime, options.decayTime, options.susPercent, options.releaseTime)
+  envelope.setRange(options.attackLevel, options.releaseLevel)
+
+  return envelope
+}
+
+export const createSoundMode = (waveOptions, envelopeOptions) => {
+  let envelope = envelope(envelopeOptions)
+  singleNote(waveOptions).amp(envelope)
+  return envelope
+}
+
