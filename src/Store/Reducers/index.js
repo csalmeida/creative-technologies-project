31cb8c86512118982feb1f65c77e76765c27e6ae
Cristiano Almeida <cs.almeida@protonmail.ch>
2019-03-24 21:50:57 +0000

    Fixed iissue where sound mapping was rejected by store.

--- src/Store/Reducers/index.js
+++ src/Store/Reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux"
 import videoStream from "./videoStream"
 import poseEstimation from "./poseEstimation"
+import soundMapping from "./soundMapping"
 
 export default combineReducers({
   videoStream,
   poseEstimation,
+  soundMapping,
 })
